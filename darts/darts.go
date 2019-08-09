184503478adcdd22df9e904adc5b85c2c817e49a
Vadim Statishin <statishin@gmail.com>
2019-08-10 00:55:56 +0300

    Implement Darts

---
+++ darts/darts.go
@@ -0,0 +1,23 @@
+// Package darts implement function Score
+package darts
+
+import "math"
+
+const (
+	outerCircle  = 10
+	middleCircle = 5
+	innerCircle  = 1
+)
+
+// Score returns points
+func Score(x, y float64) int {
+	r := math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2))
+	if r <= innerCircle {
+		return 10
+	} else if r <= middleCircle {
+		return 5
+	} else if r <= outerCircle {
+		return 1
+	}
+	return 0
+}
