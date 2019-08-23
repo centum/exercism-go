81d3e76a247757359027b209e0a8f120085a6c17
Vadim Statishin <statishin@gmail.com>
2019-08-24 00:13:37 +0300

    Add difference of squares

---
+++ difference-of-squares/difference_of_squares.go
@@ -0,0 +1,18 @@
+// Package diffsquares implement some functions
+package diffsquares
+
+// SquareOfSum returns squares of sum natural number
+func SquareOfSum(n int) int {
+	x := n * (n + 1) / 2
+	return x * x
+}
+
+// SumOfSquares return sum of squares natural number
+func SumOfSquares(n int) int {
+	return n * (n + 1) * (2 * n + 1) / 6
+}
+
+// Difference return difference between SquareOfSum and SumOfSquares
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
