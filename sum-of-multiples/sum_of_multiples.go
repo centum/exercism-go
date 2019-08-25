81d2292ced075f02e82b45c1b98cbe7f728bf899
Vadim Statishin <statishin@gmail.com>
2019-08-26 00:13:22 +0300

    Implement sum of number

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -1,5 +1,26 @@
+// Package summultiples implement SumMultiples
 package summultiples
 
-func SumMultiples(limit int, divisors... int) int {
-	return 0
+// SumMultiples return the sum of all the unique multiples of particular numbers from given number
+func SumMultiples(limit int, divisors ...int) int {
+	res := 0
+
+	for i, d := range divisors {
+		if d <= 0 {
+			continue
+		}
+		m := int((limit-1)/d) * d
+
+	LOOP1:
+		for n := m; n >= d; n -= d {
+			for k := 0; k < i; k++ {
+				if n%divisors[k] == 0 {
+					continue LOOP1
+				}
+			}
+			res += n
+		}
+	}
+
+	return res
 }
