fdf276fa1471459e83e48bd5ae03b430859c9f5f
Vadim Statishin <statishin@gmail.com>
2019-09-03 15:21:32 +0300

    Improve code luhn

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -9,36 +9,30 @@ import (
 
 // Valid return true if checksum correct luhn algorithm
 func Valid(inputCode string) bool {
-	sumNum := 0
-
 	inputCode = strings.ReplaceAll(inputCode, " ", "")
 	count := len(inputCode)
+	isEven := count%2 == 0
+	sumNum := 0
 
 	if count <= 1 {
 		return false
 	}
-
-	isEven := count%2 == 0
-
+	
 	for _, r := range inputCode {
 		if !unicode.IsDigit(r) {
 			return false
 		}
-
 		v, err := strconv.Atoi(string(r))
 		if err != nil {
 			return false
 		}
-
 		if isEven {
-			v = v * 2
+			v *= 2
 			if v > 9 {
 				v -= 9
 			}
 		}
-
 		sumNum += v
-
 		isEven = !isEven
 	}
 
