0bc8f0f6189cb28843f1e83ee406c5834c2bfa00
Vadim Statishin <statishin@gmail.com>
2019-09-03 13:48:06 +0300

    Improve luhn

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -3,54 +3,44 @@ package luhn
 
 import (
 	"strconv"
+	"strings"
 	"unicode"
 )
 
 // Valid return true if checksum correct luhn algorithm
 func Valid(inputCode string) bool {
-	sumEven := 0 // Sum if even digits
-	sumOdd := 0  // Sum if odd digits
-	count := 0   // Count digits
+	sumNum := 0
 
-	for _, r := range inputCode {
-		if unicode.IsDigit(r) {
-			v, err := strconv.Atoi(string(r))
-			if err != nil {
-				return false
-			}
-
-			d := v * 2
-			if d > 9 {
-				d -= 9
-			}
-
-			if count%2 == 0 {
-				sumEven += d
-				sumOdd += v
-				count++
-				continue
-			}
-
-			sumEven += v
-			sumOdd += d
-			count++
-			continue
-
-		}
-
-		if !unicode.IsSpace(r) {
-			return false
-		}
-
-	}
+	inputCode = strings.ReplaceAll(inputCode, " ", "")
+	count := len(inputCode)
 
 	if count <= 1 {
 		return false
 	}
 
-	if count%2 == 0 {
-		return sumEven%10 == 0
+	isEven := count%2 == 0
+
+	for _, r := range inputCode {
+		if !unicode.IsDigit(r) {
+			return false
+		}
+
+		v, err := strconv.Atoi(string(r))
+		if err != nil {
+			return false
+		}
+
+		if isEven {
+			v = v * 2
+			if v > 9 {
+				v -= 9
+			}
+		}
+
+		sumNum += v
+
+		isEven = !isEven
 	}
-	
-	return sumOdd%10 == 0
+
+	return sumNum%10 == 0
 }
