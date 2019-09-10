94d296d1fbcdcb69faffbf42dcc16a9a97a10976
Vadim Statishin <statishin@gmail.com>
2019-09-10 17:39:39 +0300

    Fix nth prime

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -1,23 +1,23 @@
 package prime
 
-import "fmt"
-
 func Nth(n int) (int, bool) {
 	if n == 0 {
 		return 0, false
 	}
-	res := 0
+	res := 1
+	k := 0
 
 LOOP0:
-	for i := 2; res < n; i++ {
-		for k := i; k > 1; k-- {
-			if i%k == 0 {
+	for {
+		res++
+		for i := res - 1; i >= 2; i-- {
+			if res%i == 0 {
 				continue LOOP0
 			}
 		}
-		res++
-		fmt.Printf("%d -> %d\n", res, i)
+		k++
+		if k == n {
+			return res, true
+		}
 	}
-
-	return res, true
 }
