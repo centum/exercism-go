9d71032ee94c3dbf4e8b337029cc616b3c0816ad
Vadim Statishin <statishin@gmail.com>
2019-09-10 17:43:42 +0300

    Improve nth prime

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -4,12 +4,16 @@ func Nth(n int) (int, bool) {
 	if n == 0 {
 		return 0, false
 	}
+	if n == 1 {
+		return 2, true
+	}
+
 	res := 1
-	k := 0
+	k := 1
 
 LOOP0:
 	for {
-		res++
+		res += 2
 		for i := res - 1; i >= 2; i-- {
 			if res%i == 0 {
 				continue LOOP0
