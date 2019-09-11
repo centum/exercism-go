04fe898487dfbabc28897b691edf79446e4ae6ff
Vadim Statishin <statishin@gmail.com>
2019-09-12 00:11:20 +0300

    Improve clock

--- clock/clock.go
+++ clock/clock.go
@@ -1,59 +1,48 @@
-// Package clock Implement a clock that handles times without dates.
+// Package clock implement a clock that handles times without dates.
 package clock
 
 import (
 	"fmt"
 )
 
-// Clock struct define clock
-type Clock struct {
-	h, m int
-}
+// Clock type define clock
+type Clock int
 
 // New return created struct Clock
 func New(h, m int) Clock {
-	c := Clock{h, m}
-	c.normalize()
+	if m <= -60 || m >= 60 {
+		hh := m / 60
+		h += hh
+		m -= hh * 60
+	}
+
+	if h <= -24 || h >= 24 {
+		h -= h / 24 * 24
+	}
+
+	if m < 0 {
+		h -= 1
+		m += 60
+	}
+
+	if h < 0 {
+		h += 24
+	}
+	c := Clock(h*60 + m)
 	return c
-}
-
-// normalize normalization method hours and minutes
-func (c *Clock) normalize() {
-	if c.m <= -60 || c.m >= 60 {
-		hh := c.m / 60
-		c.h += hh
-		c.m = c.m - hh*60
-	}
-
-	if c.m < 0 {
-		c.h -= 1
-		c.m = 60 + c.m
-	}
-
-	if c.h <= -24 || c.h >= 24 {
-		c.h = c.h - c.h/24*24
-	}
-
-	if c.h < 0 {
-		c.h = 24 + c.h
-	}
 }
 
 // String return string representation struct Clock
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.h, c.m)
+	return fmt.Sprintf("%02d:%02d", c/60, c%60)
 }
 
 // Add return new struct Clock with added minutes m
 func (c Clock) Add(m int) Clock {
-	c.m += m
-	c.normalize()
-	return c
+	return New(0, int(c)+m)
 }
 
 // Subtract return new struct Clock with subtract minutes m
 func (c Clock) Subtract(m int) Clock {
-	c.m -= m
-	c.normalize()
-	return c
+	return New(0, int(c)-m)
 }
