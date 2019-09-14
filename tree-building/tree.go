ad2c7a2800efd7eecec6f980dc73b276b4ee9dc0
Vadim Statishin <statishin@gmail.com>
2019-09-14 22:57:41 +0300

    Implement tree - two optimization - add comment

--- tree-building/tree.go
+++ tree-building/tree.go
@@ -1,3 +1,4 @@
+// Package tree implement parser tree struct
 package tree
 
 import (
@@ -14,6 +15,7 @@ type Node struct {
 	Children []*Node
 }
 
+// AppendChild append new child to Node
 func (n *Node) AppendChild(child *Node) {
 	i := sort.Search(len(n.Children), func(i int) bool { return n.Children[i].ID > child.ID })
 	n.Children = append(n.Children, &Node{})
@@ -28,13 +30,14 @@ var (
 	ErrNoRoot          = errors.New("no root node")
 )
 
+// Build build Node struct from list Record
 func Build(records []Record) (*Node, error) {
 	if len(records) == 0 {
 		return nil, nil
 	}
 
 	mapNodes := make(map[int]*Node, len(records))
-	maxID := 0
+	maxID := 0 // needed to detect non-continuous
 	for _, r := range records {
 		if _, ok := mapNodes[r.ID]; !ok {
 			mapNodes[r.ID] = &Node{ID: r.ID}
